b9902e0656371419fd9dbc0fa4a871a8ae87b7c1
Melike <m.taganova2007@gmail.com>
2025-01-25 20:58:38 +0500

    Create 63.py

---
+++ 63.py
@@ -0,0 +1,44 @@
+i = 1
+while i<=5:
+    print(i, "software engineering")
+    i+=1
+print("exit")
+
+#1 software engineering
+#2 software engineering
+#3 software engineering
+#4 software engineering
+#5 software engineering
+#exit
+
+i = 1
+while i<=5:
+    print(i, "software engineering")
+    i+=1
+    print("exit")
+#1 software engineering
+#exit
+#2 software engineering
+#exit
+#3 software engineering
+#exit
+#4 software engineering
+#exit
+#5 software engineering
+#exit
+
+i = 1
+while i<= 10:
+    print(i)
+    i+=1
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+
