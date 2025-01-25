f5ed45e71da36de202933a31fa7f045a5ead66bc
Melike <m.taganova2007@gmail.com>
2025-01-25 19:50:55 +0500

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,10 @@
+isgarler = { "Mekan" : {"hunari" : "programmist",
+"bilimi" : "orta",
+"yashy" : 22},
+"Oraz" : {"hunari" : "hasapchy",
+"bilimi" : "yokary",
+"yashy" : 30}
+}
+print(isgarler["Mekan"])
+print(isgarler["Mekan"]["hunari"])
+print(isgarler["Oraz"]['bilimi'])
