bed0484ddd10e633255b03504102ca9289596dcb
Melike <m.taganova2007@gmail.com>
2025-01-25 20:51:12 +0500

    Create 50.py

---
+++ 50.py
@@ -0,0 +1,20 @@
+n = int(input('enter a number: '))
+count = 10
+while count>= 1:
+    product= n * count
+    print(f"{n} * {count} = {product}")
+    count-= 1
+
+
+
+#enter a number: 6
+#6 * 10 = 60
+#6 * 9 = 54
+#6 * 8 = 48
+#6 * 7 = 42
+#6 * 6 = 36
+#6 * 5 = 30
+#6 * 4 = 24
+#6 * 3 = 18
+#6 * 2 = 12
+#6 * 1 = 6
