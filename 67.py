0cdf157df7bf5ade2b9c27eb5d1c6f413788504b
Melike <m.taganova2007@gmail.com>
2025-01-25 21:00:01 +0500

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,35 @@
+def gosmak():
+    netije = san1 + san2
+    print(f"Netije: {netije}")
+def ayyrmak():
+   san2 = int(input("Ikinji sanyny giriz"))
+    san3 = int(input("ucunji sanyny giriz"))
+    funksiya = input("Funksiyany saylan: +, -, *,/\nFunksiya:")
+    if funksiya == "+":
+        gosmak()
+    elif funksiya == "-":
+        ayyrmak()
+    elif funksiya == "*":
+        kopeltmek()
+    elif funksiya == "/":
+        bolmek()
+    elif san3 == 0:
+        bolmek_1()
+    else:
+        print("Nadogry funksiya")  netije = san1 - san2
+    print(f"Netije: {netije}")
+def kopeltmek():
+    netije = san1 * san2
+    print(f"Netije: {netije}")
+def bolmek():
+    netije = san1 / san2
+    print(f"Netije: {netije}")
+def bolmek_1(): 
+    netije = san1/san3
+    print(f"{netije} nola bolunenok")
+
+while True:
+    san1 = int(input("Birinji sanyny giriz"))
+   
+    
+   
