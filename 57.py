51750e49872f322b03fd2dc98f60c076ca972236
Melike <m.taganova2007@gmail.com>
2025-01-25 20:56:37 +0500

    Create 57.py

---
+++ 57.py
@@ -0,0 +1,16 @@
+n = int(input("haysy amal: "))
+a = int(input("1-nji san : "))
+b = int(input("2-nji san : "))
+if n==1:
+    print(f"{a}+{b}= {a+b}")
+elif n==2:
+    print(f"{a}-{b}= {a-b}")
+elif n==3:
+    print(f"{a}*{b}= {a*b}")
+elif n==4:
+    print(f"{a}/{b}= {a/b}")    
+
+#haysy amal: 1
+#1-nji san : 2
+#2-nji san : 3
+#2+3= 5
