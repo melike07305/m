fdde3abb28f5a38128b5b8aae41547645279a54b
Melike <m.taganova2007@gmail.com>
2025-01-25 20:51:29 +0500

    Create 51.py

---
+++ 51.py
@@ -0,0 +1,18 @@
+count= 1
+while count<5:
+    print(count)
+    count+=1
+#1
+#2
+#3
+#4
+
+n= 10
+sum = 0
+i =1
+while i<=n:
+    if (i%2 == 0):
+        sum = sum+i
+    i = i+1
+print("the sum is",sum)
+# the sum is 30
