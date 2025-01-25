6a16b7b0cadd3aa6b98559e2c0b20d240e300e30
Melike <m.taganova2007@gmail.com>
2025-01-25 20:58:02 +0500

    Create 61.py

---
+++ 61.py
@@ -0,0 +1,13 @@
+summa =0
+while True:
+    n= int(input("enter a number: "))
+    if n==0:
+        break
+    else:
+        summa+=n
+print(f"sum: {summa}")
+
+#enter a number: 6
+#enter a number: 2
+#enter a number: 0
+#sum: 8
