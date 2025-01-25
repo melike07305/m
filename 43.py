f7b7cafbe80ff833f31d6c035d50320970df9423
Melike <m.taganova2007@gmail.com>
2025-01-25 20:49:00 +0500

    Create 43.py

---
+++ 43.py
@@ -0,0 +1,9 @@
+while True:
+    number = input("enter a number: ")
+    if number =="quit":
+        print("goodbye!!!")
+    else:
+        print(f"{number} the square is {int(number)**2}")
+
+# enter a number: 3
+# 3 the suare is 9
