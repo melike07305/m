3dc629bce43e8ba1f8a203b6de7bf426ce8e48f5
Melike <m.taganova2007@gmail.com>
2025-01-25 20:51:47 +0500

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,20 @@
+num = 10
+sum = 0
+i =1
+while i<= num:
+    sum =sum +i
+    i+=1
+print("sum of first 10 number is:",sum)
+#sum of first 10 number is: 55
+
+a= 10
+while a>0:
+    print(("value of a is"),a)
+    a=a-2
+print("loap is Completed")
+#value of a is 10
+#value of a is 8
+#value of a is 6
+#value of a is 4
+#value of a is 2
+#loap is Completed
