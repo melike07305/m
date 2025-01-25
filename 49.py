361b04cbe546b76a378413ef46ec8e2261d884ad
Melike <m.taganova2007@gmail.com>
2025-01-25 20:50:52 +0500

    Create 49.py

---
+++ 49.py
@@ -0,0 +1,13 @@
+n = int(input('please enter number: '))
+while n>= 1:
+    if n%2 == 0:
+        print(f"{n} is a odd number")
+    elif n%2 ==1:
+        print(f"{n}is a even number")
+    n-=1
+# please enter number: 5
+# 5 is a even number
+# 4 is a odd number
+# 3 is a even number
+# 2 is a odd number
+# 1 is a even number
