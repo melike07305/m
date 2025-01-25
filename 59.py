7ef71b8a79e100cacc1e0145189e129002cd8268
Melike <m.taganova2007@gmail.com>
2025-01-25 20:57:16 +0500

    Create 59.py

---
+++ 59.py
@@ -0,0 +1,36 @@
+for i in range(5): #[1,2,3,4,]
+    print(i)
+#0
+#1
+#2
+#3
+#4
+for i in range(1,6):   #[1,2,3,4,5]
+    print(i)
+#1
+#2
+#3
+#4
+#5
+for i in range (1,11,2): #[1,2,3,5,7,9]
+    print(i)
+#1
+#3
+#5
+#7
+#7
+#9
+for  i in range(1,6):
+    print("introduction to software enginnering")
+#introduction t software enginnering
+#introduction t software enginnering
+#introduction t software enginnering
+#introduction t software enginnering
+#introduction t software enginnering
+for  i in range(1,6):
+    print(i,"introduction to software enginnering")
+# 1 introduction t software enginnering
+# 2 introduction t software enginnering
+# 3 introduction t software enginnering
+# 4 introduction t software enginnering
+# 5 introduction t software enginnering
