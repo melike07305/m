55acaef4d90f6004b5a0dc2621f2062971d57f68
Melike <m.taganova2007@gmail.com>
2025-01-25 19:53:05 +0500

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,128 @@
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444"}
+
+# print(a)
+print(a)
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444"}
+
+# print(a["Anna"])
+print(a["Anna"])
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444"}
+
+# key = input('Enter the word: ')
+# print(key, "- yn telefon belgisi", a [key])
+key = input('Enter the word: ')
+print(key, "- yn telefon belgisi", a [key])
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444"}
+# print(len(a))
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444"}
+print(len(a))
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444"}
+# a["Gurban"] = "63625494"
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444"}
+a["Gurban"] = "63625494"
+
+# print(a)
+print(a)
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444"}
+# a["Muhammet"] = "63384289"
+# print(a)
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444"}
+a["Muhammet"] = "63384289"
+print(a)
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444",
+# "Muhammet" : "63384289"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444",
+"Muhammet" : "63384289"}
+
+# a.pop("Gurban")
+# print(a)
+a.pop("Gurban")
+print(a)
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444",
+# "Muhammet" : "63384289"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444",
+"Muhammet" : "63384289"}
+
+# for i in a.keys():
+#     print(i)
+for i in a.keys():
+    print(i)
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444",
+# "Muhammet" : "63384289"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444",
+"Muhammet" : "63384289"}
+
+# for i in a.values():
+#     print(i)
+for i in a.values():
+    print(i)
+
+
+# a = {"Anna" : "63188199",
+# "Gurban" : "63115599",
+# "Oraz" : "62111444",
+# "Muhammet" : "63384289"}
+a = {"Anna" : "63188199",
+"Gurban" : "63115599",
+"Oraz" : "62111444",
+"Muhammet" : "63384289"}
+
+# for i, j in a.items():
+#     print(i, j)
+for i, j in a.items():
+    print(i, j)
